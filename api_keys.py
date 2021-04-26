1e20a82fcdcb49daac862c84bd52a9ebc8c10564
unknown <ryanspicuzzi@comcast.net>
2021-04-26 19:05:36 -0400

    updating weatherpy code and adding output folder with graph png's

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "29d29f0ffc83d7374a26865fb99ec7bc"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB8XGADiogE29Oy7giWG3-thceRliloI5o"
